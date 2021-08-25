b9a3aedd5ecdc2ab59fb95ee25b6091de8b8cca2
Stephen Baldwin <stephen@syncromsp.com>
2021-08-25 14:53:46 -0700

    syntax error

--- lib/index.rb
+++ lib/index.rb
@@ -88,7 +88,7 @@ def run_rubocop
   conclusion = 'success'
   count = 0
 
-  puts "Files with errors: #{errors['files'].map{|f| f['path'].join(", ")}}"
+  puts "Files with errors: #{errors['files'].map{|f| f['path']}.join(", ")}"
   errors['files'].each do |file|
     path = file['path']
     offenses = file['offenses']
